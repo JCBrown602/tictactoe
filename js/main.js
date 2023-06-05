8aad457db0c3291e4afa9e1fefda6aec1a7db431
jcbrown602 <3117301+JCBrown602@users.noreply.github.com>
2023-06-05 16:53:17 -0500

    Add file framework.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// (MODEL) Data
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+// (VIEW) All DOM action takes place here
+function render() {
+    console.log("Render...");
+}; 
+/*----- event listeners -----*/
+
+// (CONTROLLER)
+/*----- functions -----*/
+function init() {
+    console.log("Init...");
+    render();
+};
+
+
